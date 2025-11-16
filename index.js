f28ed27b48347267f1c60e38b4bc2e048b360b09
André Montenegro <126609983+andre4383@users.noreply.github.com>
2025-11-16 01:16:06 -0300

    Alterações no código, agora funcionando o timer

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-const elMinutes = document.getElementById('minutes');
-const elSeconds = document.getElementById('seconds');
-const elStart = document.getElementById('start');
-const elPause = document.getElementById('pause');
+const FOCUS_MIN = 25;
+let reimaningMs = FOCUS_MIN * 60 * 1000; // 25 minutos em ms
+let lastTick = null;
+let timerId = null;
+let isRunning = false;
 
-let status = 'stopped';
-let remaining = 1500; // 25 minutes in seconds
-let tickID = null; // Variable to store the interval ID
-let endAt = null;
-let totalSeconds = 25 * 60;
-let remainingSeconds = totalSeconds;
 
 function updateDisplay() {
-};
+    const totalSec = Math.floor(reimaningMs / 1000);
+    const m = Math.floor(totalSec / 60);
+    const s = totalSec % 60;
+
+    document.getElementById('minutes').textContent = String(m).padStart(2, '0');
+    document.getElementById('seconds').textContent = String(s).padStart(2, '0');
+}
+
+function tick() {
+    const now = Date.now();
+
+    if(lastTick === null) {
+        lastTick = now;
+    }
+
+    reimaningMs = Math.max(0, reimaningMs - (now - lastTick));
+    updateDisplay(reimaningMs);
+
+    lastTick = now;
+
+    if(reimaningMs <= 0) {
+        alert('Tempo encerrado!');
+        isRunning = false;
+        return;
+    }
+
+    // agenda próximo tick
+    timerId = setTimeout(tick, 250);
+}
+
+function start() {
+    if (isRunning) return; // já está rodando
+
+    isRunning = true;
+    lastTick = null;
+
+    timerId = setTimeout(tick, 250);
+} 
+
+
+
+document.getElementById('btn-start').addEventListener('click', start);
